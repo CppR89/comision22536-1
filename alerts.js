fe92c202ad80871c0d87da031ecaeae963f71e6a
LucianoSignorelli <luciano.signorelli@bue.edu.ar>
2022-09-27 09:08:17 -0300

    Clase 2 Javascript

---
+++ alerts.js
@@ -0,0 +1,23 @@
+
+// comisionVar = 10;
+// console.log(comisionVar);
+// console.log(comisionLet);
+// alert('Hola Mundo');
+let edad = prompt('¿Cuál es tu edad?');
+// console.log(typeof(edad));
+if( parseInt(edad) === 30){
+    alert('Tiene 30 años', edad);
+}
+else{
+    alert('Edad incorrecta');
+}
+
+// let nombre = prompt('¿Cuál es tu nombre?');
+// console.log( nombre );
+// console.log( '****' + nombre + '****' ); // ''
+
+// const seleccion = confirm('¿Está seguro de borrar esto?');
+// console.log( seleccion );
+
+
+
---
+++ scripts/primitivos.js
@@ -0,0 +1,42 @@
+
+let nombre = 'Homero Simpson';
+console.log( nombre );
+
+nombre = 'Bart Simpson';
+console.log( nombre );
+
+nombre = "Lisa Simpson";
+nombre = `Lisa Simpson`;
+
+console.log( typeof nombre );
+
+nombre = 123;
+console.log( typeof nombre );
+
+
+let esSimspon = false;
+console.log( typeof esSimspon );
+
+
+let edad = 33;
+console.log( typeof edad );
+
+edad = 33.001;
+console.log( typeof edad );
+
+
+let superPoder;
+console.log( typeof superPoder ); // ???
+
+let soyNull = null;
+console.log( typeof soyNull ); // ???
+
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log( typeof symbol1 );
+
+
+console.log( symbol1 === symbol2 );
+
