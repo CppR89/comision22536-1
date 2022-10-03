4718890b6206635efacdcc5b32d7a16a1ba8df94
innovation-hard <jorge.aguirre@outlook.com.ar>
2022-10-03 20:02:12 -0300

    nuevo

--- alerts.js
+++ alerts.js
@@ -5,8 +5,8 @@
 // alert('Hola Mundo');
 let edad = prompt('¿Cuál es tu edad?');
 // console.log(typeof(edad));
-if( parseInt(edad) === 35){
-    alert('Tiene 30 años', edad);
+if( parseInt("edad") === 35){
+    alert('Tiene 30 años');
 }
 else{
     alert('Edad incorrecta, sos mayor');
